e11a2bc8c8e8ca3bff6b008e4c6fa458a5e22edb
visual-of-bits <64715367+visual-of-bits@users.noreply.github.com>
2021-05-25 21:33:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CV-lens-distortion-profiler
+ 카메라 렌즈의 distortion을 계산하는 프로그램
---
+++ lens-prof.py
@@ -0,0 +1,69 @@
+import sys
+import cv2
+import numpy as np
+
+args = sys.argv
+frameRate = 1
+square_size = 4
+objpoints = []
+imgpoints = []
+
+
+def writefram(sec,clc):
+ vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+ bol,image = vidcap.read()
+ findcal(gx,gy,image,sec)
+ return bol
+
+
+def findcal(nx,ny,img,sc):
+ try:
+  pattern_size = (nx, ny)  
+  pattern_points = np.zeros( (np.prod(pattern_size), 3), np.float32 )
+  pattern_points[:,:2] = np.indices(pattern_size).T.reshape(-1, 2)
+  pattern_points *= square_size
+  img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+  ret, corners = cv2.findChessboardCorners(img, (nx, ny), None)    
+  if ret == True:
+        print(str(sc)+"s, processed...")
+        term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
+        cv2.cornerSubPix(img, corners, (5,5), (-1,-1), term)
+        imgpoints.append(corners.reshape(-1, 2))
+        objpoints.append(pattern_points)
+ except Exception:
+    pass
+
+#main for here
+if (len(args) != 2):  
+ print("Put video file name...")
+ quit() 
+
+#sample image
+vidcap = cv2.VideoCapture(args[1])
+vidcap.set(cv2.CAP_PROP_FPS,1)
+hav,image = vidcap.read()
+
+#chessboard profile
+print("Number of chessboard blocks vertically:",end='')
+gx=int(input())
+print("Number of chessboard blocks horizontally:",end='')
+gy=int(input())
+
+#do until video end
+sec = 0
+clock=0
+while writefram(sec,clock):
+ sec = sec + frameRate
+ clock = clock + 1
+ sec = round(sec, 4)
+ 
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, image.shape[1::-1], None, None)
+#save profile
+np.save("mtx", mtx)
+np.save("dist", dist)
+
+#display profiles
+print("RMS: ", ret)
+print("mtx: ", mtx)
+print("dis: ", dist)
+print("camera file exported")
